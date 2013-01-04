ec7cec8ff0cdf99db3efdd14f0823c6a94d57bd0
Drew Garen <bamtaumod@gmail.com>
2013-01-04 00:41:25 -0500

    Orca Vendor Props

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+add_lunch_combo orca_crespo-userdebug
+add_lunch_combo orca_d2att-userdebug
+add_lunch_combo orca_grouper-userdebug
+add_lunch_combo orca_maguro-userdebug
+add_lunch_combo orca_mako-userdebug
+add_lunch_combo orca_tilapia-userdebug
+add_lunch_combo orca_toro-userdebug
+add_lunch_combo orca_toroplus-userdebug
